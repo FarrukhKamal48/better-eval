bd903443cabc0cb5349f58d21c17b007127f602a
FarrukhKamal48 <farrukhkamal429@gmail.com>
2023-11-19 17:09:02 +0500

    Seperated lexer funcitonally to a header file

--- Calc.c
+++ Calc.c
@@ -1,66 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef int TokenType;
-enum {
-    TokenType_Star,
-    TokenType_Slash,
-    TokenType_Plus,
-    TokenType_Minus,
-
-    TokenType_EOF,
-    TokenType_ERROR
-};
-
-typedef struct string {
-    char *str;
-    int size;
-} string;
-
-typedef struct Token {
-    TokenType type;
-    char *lexeme;
-} Token;
-
-typedef struct Lexer {
-    char *start;
-    char *current;
-} Lexer;
-
-void lexer_make_lexeme (Lexer *lexer, Token *token) {
-    int len = lexer->current - lexer->start;
-    token->lexeme = (char*) malloc(len * sizeof(char));
-
-    for (int i = 0; i < lexer->current - lexer->start; ++i) {
-        token->lexeme[i] = *(lexer->start + i);
-    }
-}
-
-Token lexer_make_token (Lexer *lexer, TokenType type) {
-    Token newToken = (Token) {
-        .type = type,
-    };
-    lexer_make_lexeme(lexer, &newToken);
-
-    return newToken;
-}
-
-Token lexer_next_token (Lexer *lexer) {
-    lexer->current = lexer->start;
-
-    if (*lexer->current == '\0') 
-        return lexer_make_token(lexer, TokenType_EOF);
-
-    switch (*lexer->current) {
-        case '/': lexer->current++; return lexer_make_token(lexer, TokenType_Slash); 
-        case '*': lexer->current++; return lexer_make_token(lexer, TokenType_Star); 
-        case '+': lexer->current++; return lexer_make_token(lexer, TokenType_Plus); 
-        case '-': lexer->current++; return lexer_make_token(lexer, TokenType_Minus); 
-
-        default: return lexer_make_token(lexer, TokenType_ERROR);
-    }
-}
+#include "source/Lexer.h"
 
 int main(int argc, char *argv[])
 {
@@ -77,25 +16,17 @@ int main(int argc, char *argv[])
     };
 
     Token TokenStream[10000];
+
+    int i = 0;
     Token CurrToken = lexer_next_token(&lexer);
-    printf("%s", CurrToken.lexeme);
+    while ((CurrToken.type != TokenType_EOF & CurrToken.type != TokenType_ERROR) & (i < sizeof(TokenStream)/sizeof(TokenStream[0]))) {
+        TokenStream[i] = CurrToken;
+        CurrToken = lexer_next_token(&lexer);
+        printf("    Start: %c, Len: %d, Type: %d, Size: %lu\n", *TokenStream[i].lexeme.str, TokenStream[i].lexeme.size, TokenStream[i].type, sizeof(TokenStream[0]));
 
-    // int i;
-    // while ((CurrToken.type != TokenType_EOF & CurrToken.type != TokenType_ERROR) & (i < sizeof(TokenStream)/sizeof(TokenStream[0]))) {
-    //     TokenStream[i] = CurrToken;
-    //     CurrToken = lexer_next_token(&lexer);
-    //     i++;
-    //
-    //     printf("%s", CurrToken.lexeme);
-    // }
-
-    // char test[] = "0123456789";
-    // int start = 1;
-    // int current = 1;
-    // char lexerem[current-start+1];
-    // lexer_make1_lexeme(start, current, lexerem, test);
-    // printf("%c", lexerem[0]);
-    // printf("%c", lexerem[1]);
+        i++;
+    }
+    printf("Tokens: %d\n", i);
 
     return 0;
 }
---
+++ source/Lexer.h
@@ -0,0 +1,95 @@
+#ifndef LEXER
+#define LEXER
+
+typedef int TokenType;
+enum {
+    TokenType_Slash,
+    TokenType_Star,
+    TokenType_Plus,
+    TokenType_Minus,
+
+    TokenType_Num,
+
+    TokenType_EOF,
+    TokenType_ERROR
+};
+
+typedef struct string {
+    char *str;
+    int size;
+} string;
+
+typedef struct Token {
+    TokenType type;
+    string lexeme;
+} Token;
+
+typedef struct Lexer {
+    char *start;
+    char *current;
+} Lexer;
+
+// functions
+
+
+/* 
+ * Checks if char is a digit
+*/
+unsigned short int isDigit(char a) {
+    switch (a) {
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9': return 1;
+
+        default: return 0;
+    }
+}
+
+/* 
+ * Makes a token, using lexer span as a lexeme and type as the tokentype
+*/
+Token lexer_make_token (Lexer *lexer, TokenType type) {
+    Token newToken = (Token) {
+        .type = type,
+        .lexeme = (string) {
+            .str = lexer->start,
+            .size = (int)(lexer->current - lexer->start),
+        }
+    };
+
+    return newToken;
+}
+
+/* 
+ * Makes a number token, following the current span
+*/
+Token lexer_number_token (Lexer *lexer) {
+    while (isDigit(*lexer->current) | (*lexer->current == '.')) {
+        lexer->current += 1;
+    }
+    return lexer_make_token(lexer, TokenType_Num);
+}
+
+/* 
+ * Gives the next token
+*/
+Token lexer_next_token (Lexer *lexer) {
+    lexer->start = lexer->current;
+
+    if (*lexer->current == '\0') 
+        return lexer_make_token(lexer, TokenType_EOF);
+
+    switch (*lexer->current) {
+        case '/': lexer->current++; return lexer_make_token(lexer, TokenType_Slash); 
+        case '*': lexer->current++; return lexer_make_token(lexer, TokenType_Star); 
+        case '+': lexer->current++; return lexer_make_token(lexer, TokenType_Plus); 
+        case '-': lexer->current++; return lexer_make_token(lexer, TokenType_Minus); 
+
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9': return lexer_number_token(lexer);
+
+        default: return lexer_make_token(lexer, TokenType_ERROR);
+    }
+}
+
+
+#endif
