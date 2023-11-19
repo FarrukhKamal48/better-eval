5d472ea883a68d044c2f0c77494a5e7d8de7c9ed
FarrukhKamal48 <farrukhkamal429@gmail.com>
2023-11-19 17:13:54 +0500

    Renamed main file from Calc.c to main.c

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "source/Lexer.h"
+
+void Tokanize(Lexer *lexer, Token *TokenStream, int len) {
+    int i = 0;
+    Token CurrToken = lexer_next_token(lexer);
+    while ((CurrToken.type != TokenType_EOF & CurrToken.type != TokenType_ERROR) & (i < len)) {
+        TokenStream[i] = CurrToken;
+        CurrToken = lexer_next_token(lexer);
+        printf("    Start: %c, Len: %d, Type: %d, Size: %lu\n", *TokenStream[i].lexeme.str, TokenStream[i].lexeme.size, TokenStream[i].type, sizeof(TokenStream[0]));
+
+        i++;
+    }
+    printf("Tokens: %d\n", i);
+}
+
+int main(int argc, char *argv[])
+{
+    // origional expression
+    char exp[100];
+
+    // get the expresion
+    printf("> ");
+    scanf("%s", exp);
+
+    // initialize lexer
+    Lexer lexer = (Lexer) {
+        .start = exp,
+        .current = exp,
+    };
+
+    // final tokanized list
+    Token TokenStream[10000];
+
+    // int i = 0;
+    // Token CurrToken = lexer_next_token(&lexer);
+    // while ((CurrToken.type != TokenType_EOF & CurrToken.type != TokenType_ERROR) & (i < sizeof(TokenStream)/sizeof(TokenStream[0]))) {
+    //     TokenStream[i] = CurrToken;
+    //     CurrToken = lexer_next_token(&lexer);
+    //     printf("    Start: %c, Len: %d, Type: %d, Size: %lu\n", *TokenStream[i].lexeme.str, TokenStream[i].lexeme.size, TokenStream[i].type, sizeof(TokenStream[0]));
+    //
+    //     i++;
+    // }
+    // printf("Tokens: %d\n", i);
+
+    // Populate the token stream
+    Tokanize(&lexer, TokenStream, sizeof(TokenStream)/sizeof(TokenStream[0]));
+
+    return 0;
+}
