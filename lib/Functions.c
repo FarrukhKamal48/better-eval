b23b8241fbe8190c001db35ba234482d1c06b26a
FarrukhKamal48 <farrukhkamal429@gmail.com>
2023-11-21 14:56:39 +0500

    Made my own alternative to stdlib functions

---
+++ lib/Functions.c
@@ -0,0 +1,24 @@
+#include "Functions.h"
+
+/* 
+ * Checks if char is a digit
+*/
+unsigned short int isDigit(char a) {
+    switch (a) {
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9': return 1;
+
+        default: return 0;
+    }
+}
+
+/* 
+ * Checks if char is whitespace
+*/
+unsigned short int isWhitspace(char a) {
+    switch (a) {
+        case ' ': case '\r': case '\t': case '\n': return 1; 
+
+        default: return 0;
+    }
+}; 
---
+++ lib/Functions.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONS
+#define FUNCTIONS
+
+/* 
+ * Checks if char is a digit
+*/
+unsigned short int isDigit(char a); 
+
+/* 
+ * Checks if char is whitespace
+*/
+unsigned short int isWhitspace(char a); 
+
+#endif
