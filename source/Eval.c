81a4157885f241849b504c2002745cc3f22dfb04
FarrukhKamal48 <farrukhkamal429@gmail.com>
2023-12-03 19:18:51 +0500

    ghirh

--- source/Eval.c
+++ source/Eval.c
@@ -3,16 +3,16 @@
 #include "Eval.h"
 #include "Parser.h"
 
-float evaluate(Node *tree) {
-    switch (tree->type) {
-        case NodeType_Num: return tree->number; break;
-        case NodeType_Positive: return +evaluate(tree->unary.operand); break;
-        case NodeType_Negative: return -evaluate(tree->unary.operand); break;
-        case NodeType_Pow: return pow(evaluate(tree->binary.left), evaluate(tree->binary.right)); break;
-        case NodeType_Div: return evaluate(tree->binary.left) / evaluate(tree->binary.right); break;
-        case NodeType_Mul: return evaluate(tree->binary.left) * evaluate(tree->binary.right); break;
-        case NodeType_Add: return evaluate(tree->binary.left) + evaluate(tree->binary.right); break;
-        case NodeType_Sub: return evaluate(tree->binary.left) - evaluate(tree->binary.right); break;
+float evaluate(Node *expr) {
+    switch (expr->type) {
+        case NodeType_Num: return expr->number; break;
+        case NodeType_Positive: return +evaluate(expr->unary.operand); break;
+        case NodeType_Negative: return -evaluate(expr->unary.operand); break;
+        case NodeType_Pow: return pow(evaluate(expr->binary.left), evaluate(expr->binary.right)); break;
+        case NodeType_Div: return evaluate(expr->binary.left) / evaluate(expr->binary.right); break;
+        case NodeType_Mul: return evaluate(expr->binary.left) * evaluate(expr->binary.right); break;
+        case NodeType_Add: return evaluate(expr->binary.left) + evaluate(expr->binary.right); break;
+        case NodeType_Sub: return evaluate(expr->binary.left) - evaluate(expr->binary.right); break;
     }
     return 0;
 }
