63fe67e2ecc345534d09812d03da7465ff726049
FarrukhKamal48 <farrukhkamal429@gmail.com>
2023-12-04 20:32:25 +0500

    implicit multiplication absolute expressions (had to use [] as the

--- lib/clib.c
+++ lib/clib.c
@@ -5,18 +5,12 @@
 #include "clib.h"
 
 unsigned short int isDigit(char a) {
-    switch (a) {
-        case '0': case '1': case '2': case '3': case '4':
-        case '5': case '6': case '7': case '8': case '9': return 1;
-
-        default: return 0;
-    }
+    return a >= '0' && a <= '9';
 }
 
 unsigned short int isWhitspace(char a) {
     switch (a) {
         case ' ': case '\r': case '\t': case '\n': return 1; 
-
         default: return 0;
     }
 }; 
