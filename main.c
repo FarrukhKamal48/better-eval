2713725eebbedaa2deabba492986fe8dff1ca414
FarrukhKamal48 <farrukhkamal429@gmail.com>
2023-12-04 12:58:52 +0500

    hfhfur

--- main.c
+++ main.c
@@ -5,37 +5,40 @@
 #include "source/Eval.h"
 #include "source/Parser.h"
 
-void Calculate(string expression);
+void Calculate(string expression, unsigned short int debug);
 
 int main(int argc, char *argv[])
 {
-    if (argc == 2) {
-        string expression = strMake(argv[1]);
-        Calculate(expression);
-    }
-    else if (argc == 1) {
+    if (argc == 1) {
         char expr[100];
         while (1) {
             printf("\n>> ");
-            fgets(expr, 100, stdin);
+            Fgets(expr, 100);
             if (expr[0] == 'q') 
                 break;
-            Calculate(strMake(expr));
+            Calculate(strMake(expr), 1);
         }
+    }
+    else if (argc == 2) {
+        string expression = strMake(argv[1]);
+        Calculate(expression, 0);
+    }
+    else if (argc == 3) {
+        string expression = strMake(argv[1]);
+        Calculate(expression, *argv[2]=='d');
     }
 
     return 0;
 }
 
-void Calculate(string expression) {
+void Calculate(string expression, unsigned short int debug) {
     Parser parser;
     parser_init(&parser, expression);
     Node *tree = parser_parse_expression(&parser, Precedence_MIN);
     float answer = evaluate(tree);
 
-    expression.str[expression.size-1] = '\0';
-    printf("%s = %f\n", expression.str, answer);
+    printf("\n%s = %f\n", expression.str, answer);
 
-    parser_print_tree(tree, 0);
+    if (debug) parser_print_tree(tree, 0);
     Dealloc_Tree(tree);
 }
