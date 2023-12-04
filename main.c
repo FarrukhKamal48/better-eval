575100c14f234198cbfd90bc6375959a761014da
FarrukhKamal48 <farrukhkamal429@gmail.com>
2023-12-04 12:22:47 +0500

    pretty printing

--- main.c
+++ main.c
@@ -5,20 +5,37 @@
 #include "source/Eval.h"
 #include "source/Parser.h"
 
+void Calculate(string expression);
+
 int main(int argc, char *argv[])
 {
-    string expression = strMake(argv[1]);
-
-    Parser parser;
-    parser_init(&parser, expression);
-    Node *tree = parser_parse_expression(&parser, Precedence_MIN);
-    parser_print_tree(tree, 0);
-
-    float answer = evaluate(tree);
-    printf("\nAnswer: %f\n", answer);
-
-    Dealloc_Tree(tree);
+    if (argc == 2) {
+        string expression = strMake(argv[1]);
+        Calculate(expression);
+    }
+    else if (argc == 1) {
+        char expr[100];
+        while (1) {
+            printf("\n>> ");
+            fgets(expr, 100, stdin);
+            if (expr[0] == 'q') 
+                break;
+            Calculate(strMake(expr));
+        }
+    }
 
     return 0;
 }
 
+void Calculate(string expression) {
+    Parser parser;
+    parser_init(&parser, expression);
+    Node *tree = parser_parse_expression(&parser, Precedence_MIN);
+    float answer = evaluate(tree);
+
+    expression.str[expression.size-1] = '\0';
+    printf("%s = %f\n", expression.str, answer);
+
+    parser_print_tree(tree, 0);
+    Dealloc_Tree(tree);
+}
