c7705598e4d8bd5eb5a4ef7e9c1425f0b7b080c0
FarrukhKamal48 <farrukhkamal429@gmail.com>
2023-12-16 18:26:26 +0500

    crudely fixed identifiers (need to learn more about linked lists)

--- source/Ident.c
+++ source/Ident.c
@@ -49,26 +49,26 @@ Identifier *ident_new(char letter, float value) {
     ret->next = NULL;
     return ret;
 }
+
 Identifier *ident_find(Identifier *ident, char letter) {
     Identifier *ret;
-    while (ident != NULL) {
+    for (; ident != NULL; ident = ident->next) {
         if (ident->letter == letter || ident->letter == '\0') {
             ret = ident;
             ret->letter = letter;
             return ret;
         }
-        ident = ident->next;
-    }
+    };
     return NULL;
 }
+
 Identifier *ident_add(Identifier *ident, char letter, float value) {
     if (ident->letter == '\0') {
         ident = ident_new(letter, value);
         return ident;
     }
-    Identifier *new_ident = ident_new(letter, value);
-    new_ident->next = ident;
-    ident = new_ident;
-    
-    return ident;
+    for (; ident->next != NULL; ident = ident->next);
+    ident->next = ident_new(letter, value);
+    return ident->next;
 }
+
